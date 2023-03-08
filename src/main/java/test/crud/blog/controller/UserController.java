a3f410067fb8ae159d0643374a90340324c2f9f8
alex reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-03-07 23:05:37 -0500

    gitingore

--- src/main/java/test/crud/blog/controller/UserController.java
+++ src/main/java/test/crud/blog/controller/UserController.java
@@ -1,12 +1,16 @@
 package test.crud.blog.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import test.crud.blog.dto.UserDTO;
+import test.crud.blog.entity.Blog;
 import test.crud.blog.entity.User;
 
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController()
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -28,6 +32,11 @@ public class UserController {
     @GetMapping(value = "/login")
     public ResponseEntity<?> signIn(@RequestBody UserDTO userDTO){
         return ResponseEntity.status(HttpStatus.OK).body("exito");
+    }
+
+    @GetMapping(value = "/myPosts")
+    public ResponseEntity<List<Blog>> getMyPosts(){
+        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(new ArrayList<>());
     }
 
     @PutMapping(value = "/update_info")
--- src/main/java/test/crud/blog/dto/UserDTO.java
+++ src/main/java/test/crud/blog/dto/UserDTO.java
@@ -1,14 +1,26 @@
 package test.crud.blog.dto;
 
+import test.crud.blog.entity.User;
+
 import java.io.Serializable;
 
 public class UserDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    private Long uid;
     private String name;
     private String lastName;
     private String username;
     private String email;
     private String password;
 
+    public UserDTO(User user){
+        super();
+        this.uid = user.getUid();
+        this.name = user.getName();
+        this.lastName = user.getLastName();
+        this.username = user.getUsername();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+    }
 }
